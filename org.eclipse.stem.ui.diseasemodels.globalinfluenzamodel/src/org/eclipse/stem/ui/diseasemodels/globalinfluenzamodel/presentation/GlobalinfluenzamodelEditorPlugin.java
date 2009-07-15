2061db7c6f32c9660f660ec7bae9b9e44b4b2dac
jkaufman <jkaufman@92a21009-5b66-0410-b83a-dc787c41c6e9>
2009-07-15 18:49:22 +0000

    move to trunk
    git-svn-id: http://dev.eclipse.org/svnroot/technology/org.eclipse.stem/trunk@162 92a21009-5b66-0410-b83a-dc787c41c6e9

---
+++ org.eclipse.stem.ui.diseasemodels.globalinfluenzamodel/src/org/eclipse/stem/ui/diseasemodels/globalinfluenzamodel/presentation/GlobalinfluenzamodelEditorPlugin.java
@@ -0,0 +1,107 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package org.eclipse.stem.ui.diseasemodels.globalinfluenzamodel.presentation;
+
+import org.eclipse.emf.common.EMFPlugin;
+
+import org.eclipse.emf.common.ui.EclipseUIPlugin;
+
+import org.eclipse.emf.common.util.ResourceLocator;
+
+import org.eclipse.emf.ecore.provider.EcoreEditPlugin;
+
+import org.eclipse.stem.core.common.provider.CoreEditPlugin;
+
+import org.eclipse.stem.definitions.edges.provider.DefinitionsEditPlugin;
+
+import org.eclipse.stem.diseasemodels.standard.provider.DiseasemodelsEditPlugin;
+
+/**
+ * This is the central singleton for the Globalinfluenzamodel editor plugin.
+ * <!-- begin-user-doc -->
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public final class GlobalinfluenzamodelEditorPlugin extends EMFPlugin {
+	/**
+	 * Keep track of the singleton.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static final GlobalinfluenzamodelEditorPlugin INSTANCE = new GlobalinfluenzamodelEditorPlugin();
+	
+	/**
+	 * Keep track of the singleton.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	private static Implementation plugin;
+
+	/**
+	 * Create the instance.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public GlobalinfluenzamodelEditorPlugin() {
+		super
+			(new ResourceLocator [] {
+				DiseasemodelsEditPlugin.INSTANCE,
+				CoreEditPlugin.INSTANCE,
+				EcoreEditPlugin.INSTANCE,
+				DefinitionsEditPlugin.INSTANCE,
+			});
+	}
+
+	/**
+	 * Returns the singleton instance of the Eclipse plugin.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return the singleton instance.
+	 * @generated
+	 */
+	@Override
+	public ResourceLocator getPluginResourceLocator() {
+		return plugin;
+	}
+	
+	/**
+	 * Returns the singleton instance of the Eclipse plugin.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return the singleton instance.
+	 * @generated
+	 */
+	public static Implementation getPlugin() {
+		return plugin;
+	}
+	
+	/**
+	 * The actual implementation of the Eclipse <b>Plugin</b>.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static class Implementation extends EclipseUIPlugin {
+		/**
+		 * Creates an instance.
+		 * <!-- begin-user-doc -->
+		 * <!-- end-user-doc -->
+		 * @generated
+		 */
+		public Implementation() {
+			super();
+	
+			// Remember the static instance.
+			//
+			plugin = this;
+		}
+	}
+
+}
