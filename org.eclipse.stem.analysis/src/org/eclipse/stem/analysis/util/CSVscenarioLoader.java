963600a55e2e414d24d50b57488f9f7dca227a71
sedlund <sedlund@92a21009-5b66-0410-b83a-dc787c41c6e9>
2010-03-03 19:27:05 +0000

    Fix remove of system files
    git-svn-id: http://dev.eclipse.org/svnroot/technology/org.eclipse.stem/trunk@637 92a21009-5b66-0410-b83a-dc787c41c6e9

--- org.eclipse.stem.analysis/src/org/eclipse/stem/analysis/util/CSVscenarioLoader.java
+++ org.eclipse.stem.analysis/src/org/eclipse/stem/analysis/util/CSVscenarioLoader.java
@@ -252,6 +252,7 @@ public class CSVscenarioLoader {
 		for(int i = 0; i < diseaseData.size(); i ++) {
 			File f = diseaseData.get(i);
 			String name = f.getName();
+			if(name.startsWith(".")) continue; //skip system files
 			int _idx = name.lastIndexOf('_');
 			int dotidx = name.lastIndexOf('.');
 			if(_idx <0 || dotidx < 0 || dotidx < _idx) continue; // not a data file
