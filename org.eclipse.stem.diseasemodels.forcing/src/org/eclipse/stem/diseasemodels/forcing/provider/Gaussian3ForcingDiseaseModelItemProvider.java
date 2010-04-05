09c2b994fd2cade1c33accc4f5f937cd0552c90a
sedlund <sedlund@92a21009-5b66-0410-b83a-dc787c41c6e9>
2010-04-05 18:22:58 +0000

    Add copyright
    git-svn-id: http://dev.eclipse.org/svnroot/technology/org.eclipse.stem/trunk@685 92a21009-5b66-0410-b83a-dc787c41c6e9

--- org.eclipse.stem.diseasemodels.forcing/src/org/eclipse/stem/diseasemodels/forcing/provider/Gaussian3ForcingDiseaseModelItemProvider.java
+++ org.eclipse.stem.diseasemodels.forcing/src/org/eclipse/stem/diseasemodels/forcing/provider/Gaussian3ForcingDiseaseModelItemProvider.java
@@ -1,10 +1,15 @@
-/**
- * <copyright>
- * </copyright>
- *
- * $Id$
- */
 package org.eclipse.stem.diseasemodels.forcing.provider;
+
+/*******************************************************************************
+ * Copyright (c) 2010 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
 
 
 import java.util.Collection;
