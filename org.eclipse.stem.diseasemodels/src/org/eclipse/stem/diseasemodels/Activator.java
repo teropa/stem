a107c04413936a2f920513448e10787668b31a46
jkaufman <jkaufman@92a21009-5b66-0410-b83a-dc787c41c6e9>
2010-06-03 17:56:25 +0000

    ASSIGNED - bug 199800: Parts of the STEM code base perform direct I/O on System.out and System.err https://bugs.eclipse.org/bugs/show_bug.cgi?id=199800 Added Helper Method to Activator to facilitate replacement of ALL System.out.println code overload method logInformation so it is not necessary to pass the error (when error is null anyway).
    git-svn-id: http://dev.eclipse.org/svnroot/technology/org.eclipse.stem/trunk@767 92a21009-5b66-0410-b83a-dc787c41c6e9

--- org.eclipse.stem.diseasemodels/src/org/eclipse/stem/diseasemodels/Activator.java
+++ org.eclipse.stem.diseasemodels/src/org/eclipse/stem/diseasemodels/Activator.java
@@ -114,21 +114,46 @@ public class Activator extends AbstractUIPlugin {
 	 *            the associated exception, or null
 	 */
 	public static void logInformation(String message, Throwable exception) {
-		// Running as a plugin?
+		// Do we have a plugin?
 		if (plugin != null) {
 			// Yes
 			plugin.getLog().log(
-					new Status(IStatus.INFO, plugin.getBundle()
-							.getSymbolicName(), 0, message, exception));
-		} else {
+					new Status(IStatus.INFO, plugin.getBundle().getSymbolicName(),
+							0, message, exception));
+		}
+		else {
+			// No
 			System.out.println(message);
 			// Exception?
 			if (exception != null) {
 				// Yes
 				System.out.println(exception.getMessage());
-				exception.printStackTrace(System.err);
-			}
+			} // if 
 		}
-
 	} // logInformation
+	
+	
+	/**
+	 * Log information to the ILog for this plugin
+	 * The method is overloaded to allow for logInformation without an error 
+	 * method sets the error to null. This should be used instead of System.out.println
+	 * throughout the code.
+	 * @param message
+	 *            the localized information message text
+	 *
+	 */
+	public static void logInformation(String message) {
+		// Do we have a plugin?
+		if (plugin != null) {
+			// Yes
+			plugin.getLog().log(
+					new Status(IStatus.INFO, plugin.getBundle().getSymbolicName(),
+							0, message, null));
+		}
+		else {
+			// No
+			System.out.println(message);
+		}
+	} // logInformation
+	
 } // Activator
