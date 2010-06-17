5087fb855d81ed2538d42487230ca901c5a7a163
sedlund <sedlund@92a21009-5b66-0410-b83a-dc787c41c6e9>
2010-06-17 22:26:45 +0000

    Fix
    git-svn-id: http://dev.eclipse.org/svnroot/technology/org.eclipse.stem/trunk@811 92a21009-5b66-0410-b83a-dc787c41c6e9

--- org.eclipse.stem.core/src/org/eclipse/stem/core/Utility.java
+++ org.eclipse.stem.core/src/org/eclipse/stem/core/Utility.java
@@ -143,7 +143,7 @@ public class Utility {
 		// ZZZ is special, it means the world
 		if(key.equalsIgnoreCase("ZZZ")) return -1;
 		// null means unspecified and truly global for population models.
-		if(key == null) return -2;
+		if(key == null || key.trim().equals("")) return -2;
 		
 		int level = 0;
 		int start = 0;
