f16e23c93a03703de76c212bdc0171baae5dc042
mdavis <mdavis@92a21009-5b66-0410-b83a-dc787c41c6e9>
2010-08-18 23:27:55 +0000

    FindBugs:  Removing dead stores
    git-svn-id: http://dev.eclipse.org/svnroot/technology/org.eclipse.stem/trunk@962 92a21009-5b66-0410-b83a-dc787c41c6e9

--- org.eclipse.stem.ui/src/org/eclipse/stem/ui/preferences/SolverPreferencePage.java
+++ org.eclipse.stem.ui/src/org/eclipse/stem/ui/preferences/SolverPreferencePage.java
@@ -39,23 +39,23 @@ import org.eclipse.ui.IWorkbenchPreferencePage;
  */
 
 public class SolverPreferencePage extends
-		FieldEditorPreferencePage implements IWorkbenchPreferencePage {
+FieldEditorPreferencePage implements IWorkbenchPreferencePage {
 
 	/**
 	 * This is the default number of threads to use when running
 	 * a simulation. The default is used if the number of CPUs cannot
 	 * be detected
 	 */
-	
+
 	public static final int DEFAULT_SIMULATION_THREADS = 2;
-	
+
 	/**
 	 * The default solver. Finite difference solver is
 	 * always assumed to be available.
 	 */
-	
+
 	public static final String DEFAULT_SOLVER = "org.eclipse.stem.solvers.fd.impl.FiniteDifferenceImpl";
-	
+
 	/**
 	 * Constructor
 	 */
@@ -76,25 +76,25 @@ public class SolverPreferencePage extends
 		final IntegerFieldEditor simulationThreadsFieldEditor = new IntegerFieldEditor(
 				PreferenceConstants.SIMULATION_THREADS,
 				Messages.getString("DMPPageSimThreads"), getFieldEditorParent()); //$NON-NLS-1$
-		
+
 		addField(simulationThreadsFieldEditor);
-		
-		
+
+
 		Solver [] solvers = getSolvers();
 		String [][] namesAndValues = new String[solvers.length][solvers.length];
-		
+
 		for(int i=0;i<solvers.length;++i) {
 			namesAndValues[i][0] = solvers[i].getClass().getSimpleName();
 			namesAndValues[i][1] = solvers[i].getClass().getName();
 		}
-		
+
 		final ComboFieldEditor comboFieldEditor = new ComboFieldEditor(
 				PreferenceConstants.DEFAULT_SOLVER,
 				Messages.getString("DMPPageDefaultSolver"),
 				namesAndValues, getFieldEditorParent());
-		
+
 		addField(comboFieldEditor);
-		
+
 	} // createFieldEditors
 
 	/**
@@ -102,37 +102,37 @@ public class SolverPreferencePage extends
 	 * 
 	 * @return Solver [] Available solvers
 	 */
-	
+
 	public static org.eclipse.stem.core.solver.Solver [] getSolvers() {
 		Solver [] solvers;
 		final IExtensionRegistry registry = Platform.getExtensionRegistry();
 		final IConfigurationElement[] solverConfigElements = registry
-				.getConfigurationElementsFor(org.eclipse.stem.core.Constants.ID_SOLVER_EXTENSION_POINT);
+		.getConfigurationElementsFor(org.eclipse.stem.core.Constants.ID_SOLVER_EXTENSION_POINT);
 
 		final List<Solver> temp = new ArrayList<Solver>();
 
-		solvers = new Solver[solverConfigElements.length];
+		//solvers = new Solver[solverConfigElements.length];
 
 		for (int i = 0; i < solverConfigElements.length; i++) {
 			final IConfigurationElement element = solverConfigElements[i];
 			// Does the element specify the class of the disease model?
-				if (element.getName().equals(Constants.SOLVER_ELEMENT)) {
-					// Yes
-					try {
-						temp.add((Solver) element
-								.createExecutableExtension("class")); //$NON-NLS-1$
-					} catch (final CoreException e) {
-						Activator.logError(
-								"Can't create solver", e); //$NON-NLS-1$
-					}
-				} // if
-			} // for each configuration element
+			if (element.getName().equals(Constants.SOLVER_ELEMENT)) {
+				// Yes
+				try {
+					temp.add((Solver) element
+							.createExecutableExtension("class")); //$NON-NLS-1$
+				} catch (final CoreException e) {
+					Activator.logError(
+							"Can't create solver", e); //$NON-NLS-1$
+				}
+			} // if
+		} // for each configuration element
 
-			solvers = temp.toArray(new Solver[] {});
+		solvers = temp.toArray(new Solver[] {});
 
 		return solvers;
 	}
-	
+
 	/**
 	 * @see org.eclipse.ui.IWorkbenchPreferencePage#init(org.eclipse.ui.IWorkbench)
 	 */
