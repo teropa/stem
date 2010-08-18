3d4a487190b0290f7cf2694827fa7ba111014596
mdavis <mdavis@92a21009-5b66-0410-b83a-dc787c41c6e9>
2010-08-18 23:44:48 +0000

    FindBugs:  inefficient map iteration
    git-svn-id: http://dev.eclipse.org/svnroot/technology/org.eclipse.stem/trunk@992 92a21009-5b66-0410-b83a-dc787c41c6e9

--- org.eclipse.stem.util.analysis/src/org/eclipse/stem/util/analysis/ModelParameters.java
+++ org.eclipse.stem.util.analysis/src/org/eclipse/stem/util/analysis/ModelParameters.java
@@ -8,6 +8,7 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
 
 
@@ -21,20 +22,20 @@ import java.util.Set;
  * Contributors:
  *     IBM Corporation - initial API and implementation
  *******************************************************************************/
-   
+
 
 /**
  * This class holds all the parameters that we might want to estimate
  * and their standard deviations and variances.
  */
 public class ModelParameters {
-	
+
 	/**
 	 * All model parameters are stored in this map
 	 */
 	private final HashMap<String, Parameter> parameters = new HashMap<String, Parameter>();
-			
-	
+
+
 	/**
 	 * All selected data as a List<double[][]> keyed by location
 	 * for display. The double indices are data[state][chartIndex][time]
@@ -42,7 +43,7 @@ public class ModelParameters {
 	 * chart index selected for each of the state variables S,E,I,R (chartIndex 0-3)
 	 */
 	private final HashMap<String, List<double[][]> > selectedData = new HashMap<String, List<double[][]>>();
-	
+
 	/**
 	 * All selected regression as a List<double[][]> keyed by location
 	 * for display. The double indices are data[state][time]
@@ -50,7 +51,7 @@ public class ModelParameters {
 	 * chart index selected for each of the state equations (chartIndex 0-3)
 	 */
 	private final HashMap<String, List<double[][]> > regressionData = new HashMap<String, List<double[][]>>();
-			
+
 
 	/**
 	 * All selected fits to the regression as a List<double[][]> keyed by location
@@ -59,17 +60,17 @@ public class ModelParameters {
 	 * chart index selected for each of the state equations (chartIndex 0-3)
 	 */
 	private final HashMap<String, List<double[][]> > fittedData = new HashMap<String, List<double[][]>>();
-			
 
-	
-	
+
+
+
 	/**
 	 * the reference population density
 	 * required to do an average across locations with varying density
 	 */
 	public static final double REFERENCE_POPULATION_DENSITY = 100.0;
-	
-	
+
+
 	/**
 	 * empty constructor
 	 */
@@ -77,23 +78,22 @@ public class ModelParameters {
 		// Nothing here
 	}
 
-	
+
 	/**
 	 * average the data in a map of model parameters
 	 * @param parameterIdMap
 	 * @return the averaged modelParameters
 	 */
 	public ModelParameters average(Map<String,ModelParameters> parameterIdMap) {
-		
-		
-		Iterator<String> iter = parameterIdMap.keySet().iterator();
-		
-		while((iter!=null)&&(iter.hasNext())) {
-			String key = iter.next();
-			ModelParameters parms = parameterIdMap.get(key);
+		//Iterator<String> iter = parameterIdMap.keySet().iterator();
+		for (Entry<String,ModelParameters> entry : parameterIdMap.entrySet()) {
+
+			//while((iter!=null)&&(iter.hasNext())) {
+			//String key = iter.next();
+			ModelParameters parms = entry.getValue();
 			this.accumulateNumerator(parms);
 			this.accumulateDenominator(parms);
-			
+
 			// and for display purposes save the data, by location, that went into the fit
 			Iterator<String> iter2 = parms.selectedData.keySet().iterator();
 			while((iter2!=null)&&(iter2.hasNext())) {
@@ -108,7 +108,7 @@ public class ModelParameters {
 				List<double[][]> data = parms.regressionData.get(id);
 				this.regressionData.put(id, data);
 			}	
-			
+
 			// and for display purposes save the fitted data, by location, that went into the fit
 			Iterator<String> iter4 = parms.fittedData.keySet().iterator();
 			while((iter4!=null)&&(iter4.hasNext())) {
@@ -121,17 +121,17 @@ public class ModelParameters {
 		this.calculateStdAvg();
 		return this;
 	}
-	
+
 
 	HashMap <String, Double> numerators = new HashMap<String, Double>();
-	
+
 	/**
 	 * accumulate numerator values from another ModelParameters instance for calculating average
 	 * @param other
 	 */
 	@SuppressWarnings("boxing")
 	private void accumulateNumerator(ModelParameters other) {
-		
+
 		for(Parameter parm : other.getParameters().values()) {
 			if(numerators.containsKey(parm.name))
 				numerators.put(parm.name, 
@@ -141,16 +141,16 @@ public class ModelParameters {
 						parm.value / parm.variance);
 		}
 	}
-	
+
 	HashMap <String, Double> denominators = new HashMap<String, Double>();
-	
+
 	/**
 	 * accumulate denominator values from another ModelParameters instance for calculating average
 	 * @param other
 	 */
 	@SuppressWarnings("boxing")
 	private void accumulateDenominator(ModelParameters other) {
-		
+
 		for(Parameter parm : other.getParameters().values()) {
 			if(denominators.containsKey(parm.name))
 				denominators.put(parm.name, 
@@ -160,7 +160,7 @@ public class ModelParameters {
 						(1.0 / parm.variance));
 		}
 	}
-	
+
 	/**
 	 * divide to get average
 	 */
@@ -171,7 +171,7 @@ public class ModelParameters {
 			this.addParameter(parm, new Parameter(parm, num / denom  ));
 		}// for
 	}// average
-	
+
 	/**
 	 * find std of each parameter average
 	 */
@@ -181,9 +181,9 @@ public class ModelParameters {
 			Parameter p = this.getParameter(parm);
 			p.stddev = Math.sqrt(1.0 / denominators.get(parm));
 		}
-		
+
 		// I assume the += below was a bug? - SED
-/*		stdBeta += Math.sqrt(1/denominatorBeta);
+		/*		stdBeta += Math.sqrt(1/denominatorBeta);
 		stdAlpha += Math.sqrt(1/denominatorAlpha);
 		stdGamma += Math.sqrt(1/denominatorGamma);
 		stdEpsilon += Math.sqrt(1/denominatorEpsilon);
@@ -192,10 +192,10 @@ public class ModelParameters {
 		stdGamma2 += Math.sqrt(1/denominatorGamma2);
 		stdEpsilon2 += Math.sqrt(1/denominatorEpsilon2);
 		stdLambda += Math.sqrt(1/denominatorLambda);
-	*/
+		 */
 	}
 
-	
+
 	/**
 	 * add a new parameter to the map
 	 * @param name Parameter name
@@ -204,7 +204,7 @@ public class ModelParameters {
 	public void addParameter(String name, Parameter parm) {
 		this.parameters.put(name, parm);
 	}
-	
+
 	/**
 	 * add selected data for display
 	 * @param name location name
@@ -213,7 +213,7 @@ public class ModelParameters {
 	public void addSelectedData(String name, List<double[][]> data) {
 		this.selectedData.put(name, data);
 	}
-	
+
 	/**
 	 * add selected regression for display
 	 * @param name location name
@@ -222,8 +222,8 @@ public class ModelParameters {
 	public void addRegressionData(String name, List<double[][]> data) {
 		this.regressionData.put(name, data);
 	}
-	
-	
+
+
 	/**
 	 * add fitted data for display
 	 * @param name location name
@@ -232,7 +232,7 @@ public class ModelParameters {
 	public void addFittedData(String name, List<double[][]> data) {
 		this.fittedData.put(name, data);
 	}
-	
+
 
 	/**
 	 * Returns the raw data (state 0) and the selected data (state 1) 
@@ -248,7 +248,7 @@ public class ModelParameters {
 		// pick the right data for the right chart
 		return data[state];
 	}
-	
+
 	/**
 	 * Returns the regression data (x = state 0, y = state 1) 
 	 * for each of the state equations (chartIndex 0-3)
@@ -263,7 +263,7 @@ public class ModelParameters {
 		// pick the right data for the right chart
 		return data[state];
 	}
-	
+
 	/**
 	 * Returns the fitted data (x = state 0, y = state 1) 
 	 * for each of the state equations (chartIndex 0-3)
@@ -278,7 +278,7 @@ public class ModelParameters {
 		// pick the right data for the right chart
 		return data[state];
 	}
-	
+
 	/**
 	 * validate the key
 	 * @param id
@@ -289,7 +289,7 @@ public class ModelParameters {
 		if ((id != null)&&(selectedData.containsKey(id))) retVal = true;
 		return retVal;
 	}
-	
+
 	/**
 	 * Returns the location IDs for which we analyzed data and
 	 * stored the lists of selected data
@@ -308,24 +308,24 @@ public class ModelParameters {
 		Arrays.sort(ids);
 		return ids;
 	}
-	
-	
-	
+
+
+
 	/**
 	 * Return the parameters
 	 * @return Map<String, Parameter> The parameters
 	 */
-	
+
 	public Map<String, Parameter> getParameters() {
 		return this.parameters;
 	}
-	
+
 	/**
 	 * Returns the specified parameter
 	 * @param p The parameter name
 	 * @return Parameter 
 	 */
-	
+
 	public Parameter getParameter(String p) {
 		return this.parameters.get(p);
 	}
@@ -335,7 +335,7 @@ public class ModelParameters {
 	 * @param p The parameter
 	 * @return boolean True if p is in the model
 	 */
-	
+
 	public boolean containsParameter(String p) {
 		return this.parameters.containsKey(p);
 	}
@@ -346,7 +346,7 @@ public class ModelParameters {
 	@Override
 	public String toString() {
 		StringBuffer buffer = new StringBuffer();
-		
+
 		for(Parameter parm:this.parameters.values()) {
 			buffer.append(parm.name).append("=").append(parm.value).append("\n");
 			buffer.append("deviation(").append(parm.name).append(")").append("=").append(parm.stddev).append("\n");
