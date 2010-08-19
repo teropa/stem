da77eb7756f729c1afdfaf8f52a4b618b9fedfa3
mdavis <mdavis@92a21009-5b66-0410-b83a-dc787c41c6e9>
2010-08-19 18:41:19 +0000

    FindBugs:  Fixing unclosed resources
    git-svn-id: http://dev.eclipse.org/svnroot/technology/org.eclipse.stem/trunk@1012 92a21009-5b66-0410-b83a-dc787c41c6e9

--- org.eclipse.stem.internal.data/src/org/eclipse/stem/internal/data/utility/DataUtility.java
+++ org.eclipse.stem.internal.data/src/org/eclipse/stem/internal/data/utility/DataUtility.java
@@ -18,6 +18,7 @@ package org.eclipse.stem.internal.data.utility;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.HashSet;
@@ -30,23 +31,23 @@ import java.util.Set;
  * 
  **/
 public class DataUtility {
-	
+
 	private static final String PARENT_DIR 		= "./resources/data/country";
 	private static final String POP_FILE_SUFFIX = "population.properties";
 	private static final String AREA_FILE_SUFFIX = "area.properties";
 	private static final String NODE_FILE_SUFFIX = "node.properties";
 	private static final String ADMIN_LEVEL 	= "ADMIN_LEVEL";
-	
+
 	/**
 	 * zero population finder
 	 */
 	public static final int FIND_ZERO_POPULATION = 0;
-	
+
 	/**
 	 * find duplicate ids
 	 */
 	public static final int FIND_DUPLICATE_IDS   = 1;
-	
+
 
 	/**
 	 * Main method for testing
@@ -61,80 +62,88 @@ public class DataUtility {
 	 * This Method scans STEM property files and runs various optional tests
 	 */
 	static void scanFiles(int option) {
-		 
-		  String record;  
-          int recCount = 0;
-          try { 
-        	  File f = new File(PARENT_DIR);
-        	  if (f.isDirectory()) {
-        		 File[] children = f.listFiles();
-        		 for (int i = 0; i < children.length; i ++) {
-        			 File child = children[i];
-        			 
-        			 if ((!child.getName().equalsIgnoreCase("ZZZ"))||(!child.getName().equalsIgnoreCase("ZZZ")) ) {
-        				 if (child.isDirectory()) {
-        					 File[] files = child.listFiles();
-        					 int poptotal = 0;
-        					 for (int j = 0; j < files.length; j ++) {
-        						 File next = files[j];
-        						 poptotal = 0;
-        						 String name = next.getName();
-        						 if( (name.indexOf("RUS")>=0) && (name.indexOf(AREA_FILE_SUFFIX)>=1) ) checkAreas(next);
-        					
-        						 if(option==FIND_DUPLICATE_IDS) {
-        							 if(name.indexOf(NODE_FILE_SUFFIX)>=0) checkDuplicates(next) ;
-        						 }
-        						 
 
-        						 if(option==FIND_ZERO_POPULATION) {
-        							 if(name.indexOf(POP_FILE_SUFFIX)>=2) {
-          							   FileInputStream fis = new FileInputStream(next); 
-          					           BufferedReader d = new BufferedReader(new InputStreamReader(fis));
-          					           while ( (record=d.readLine()) != null ) {
-          					        	 //  System.out.println(""+name+":   "+record);
-          					                 recCount++;
-          					                 
-          					                 if (record.indexOf(ADMIN_LEVEL)==-1) {
-          					                 if(record.indexOf("= 0")>= 1) {
-          					                		 System.out.println(""+name+":   "+record);
-          					                 } else if (record.indexOf(" = ")>= 1) {
-          					                		 int idx = record.indexOf("=");
-          					                		 String val = record.substring(idx+1, record.length());
-          					                		 
-          					                		 try {
-          					                		 Integer obj = new Integer(val.trim());
-          					                		 poptotal += obj.intValue();
-          					                		 } catch(NumberFormatException nfe) {
-          					                			 // nothing
-          					                		 }
-          					                 }
-          					                	 
-          					                 }
-          					           } // while
-          					          
-          					           // USE THIS FOR ZERO POPULATION REPAIR
-          					           //
-          					 // System.out.println(""+name+" pop= "+poptotal);
-          					           //
-          					           //
-          						 }
-        						 }// find zero populations
-        						 
-        						 
-        					 }// for j files
-        				 }// if directory
-        			 }// if name
-        				
-        		 }
-        	  }
-        	 
-           } catch (Exception e) { 
-              // catch io errors from FileInputStream or readLine() 
-              System.out.println(" IOException error!" + e.getMessage()); 
-              e.printStackTrace();
-           }
+		String record;  
+		int recCount = 0;
+		BufferedReader reader = null;
+		try { 
+			File f = new File(PARENT_DIR);
+			if (f.isDirectory()) {
+				File[] children = f.listFiles();
+				for (int i = 0; i < children.length; i ++) {
+					File child = children[i];
+
+					if ((!child.getName().equalsIgnoreCase("ZZZ"))||(!child.getName().equalsIgnoreCase("ZZZ")) ) {
+						if (child.isDirectory()) {
+							File[] files = child.listFiles();
+							int poptotal = 0;
+							for (int j = 0; j < files.length; j ++) {
+								File next = files[j];
+								poptotal = 0;
+								String name = next.getName();
+								if( (name.indexOf("RUS")>=0) && (name.indexOf(AREA_FILE_SUFFIX)>=1) ) checkAreas(next);
+
+								if(option==FIND_DUPLICATE_IDS) {
+									if(name.indexOf(NODE_FILE_SUFFIX)>=0) checkDuplicates(next) ;
+								}
+
+
+								if(option==FIND_ZERO_POPULATION) {
+									if(name.indexOf(POP_FILE_SUFFIX)>=2) {
+										reader = new BufferedReader(new FileReader(next));
+										while ( (record=reader.readLine()) != null ) {
+											//  System.out.println(""+name+":   "+record);
+											recCount++;
+
+											if (record.indexOf(ADMIN_LEVEL)==-1) {
+												if(record.indexOf("= 0")>= 1) {
+													System.out.println(""+name+":   "+record);
+												} else if (record.indexOf(" = ")>= 1) {
+													int idx = record.indexOf("=");
+													String val = record.substring(idx+1, record.length());
+
+													try {
+														Integer obj = new Integer(val.trim());
+														poptotal += obj.intValue();
+													} catch(NumberFormatException nfe) {
+														// nothing
+													}
+												}
+
+											}
+										} // while
+
+										// USE THIS FOR ZERO POPULATION REPAIR
+										//
+										// System.out.println(""+name+" pop= "+poptotal);
+										//
+										//
+									}
+								}// find zero populations
+
+
+							}// for j files
+						}// if directory
+					}// if name
+
+				}
+			}
+
+		} catch (Exception e) { 
+			// catch io errors from FileInputStream or readLine() 
+			System.out.println(" IOException error!" + e.getMessage()); 
+			e.printStackTrace();
+		}  finally {
+			try {
+				if (reader != null) {
+					reader.close();
+				}
+			} catch (Exception e) {
+				// Ignore exceptions on close
+			}
+		}
 	} // read codes
-	
+
 	/**
 	 * check a file for zero areas
 	 * @param file
@@ -143,85 +152,103 @@ public class DataUtility {
 		int recCount = 0;
 		double areaTotal = 0.0;
 		String record;
+
+		BufferedReader reader = null;
 		try {
-			FileInputStream fis = new FileInputStream(file);
-			BufferedReader d = new BufferedReader(new InputStreamReader(fis));
-		
-		while ((record = d.readLine()) != null) {
-			//  System.out.println(""+name+":   "+record);
-			recCount++;
+			reader = new BufferedReader(new FileReader(file));
 
-			if (record.indexOf(ADMIN_LEVEL) == -1) {
-				if (record.indexOf("= 0") >= 1) {
-					// System.out.println(""+name+":   "+record);
-				} else if (record.indexOf(" = ") >= 1) {
-					int idx = record.indexOf("=");
-					String val = record.substring(idx + 1, record.length());
+			while ((record = reader.readLine()) != null) {
+				//  System.out.println(""+name+":   "+record);
+				recCount++;
 
-					try {
-						Double obj = new Double(val.trim());
-						areaTotal += obj.intValue();
-					} catch (NumberFormatException nfe) {
-						// nothing
+				if (record.indexOf(ADMIN_LEVEL) == -1) {
+					if (record.indexOf("= 0") >= 1) {
+						// System.out.println(""+name+":   "+record);
+					} else if (record.indexOf(" = ") >= 1) {
+						int idx = record.indexOf("=");
+						String val = record.substring(idx + 1, record.length());
+
+						try {
+							Double obj = new Double(val.trim());
+							areaTotal += obj.intValue();
+						} catch (NumberFormatException nfe) {
+							// nothing
+						}
 					}
-				}
 
-			}
-		} // while
+				}
+			} // while
 		} catch (Exception e) {
 			// catch io errors from FileInputStream or readLine() 
 			System.out.println(" IOException error!" + e.getMessage());
 			e.printStackTrace();
+		} finally {
+			try {
+				if (reader != null) {
+					reader.close();
+				}
+			} catch (Exception e) {
+				// Ignore exceptions on close
+			}
 		}
 		System.out.println("*******************   " + file.getName()
 				+ " AREA = " + areaTotal);
 
 	}// CHECK AREAS
-	
+
 	/**
 	 * check a file for duplicate ids
 	 * @param file
 	 */
 	private static void checkDuplicates(File file) {
-		
+
 		int recCount = 0;
 		Set<String> keySet = new HashSet<String>();
 		Set<String> duplicateSet = new HashSet<String>();
 		String record;
+
+		BufferedReader reader = null;
 		try {
-			FileInputStream fis = new FileInputStream(file);
-			BufferedReader d = new BufferedReader(new InputStreamReader(fis));
-		
-		while ((record = d.readLine()) != null) {
-			//  System.out.println(""+name+":   "+record);
-			recCount++;
+			reader = new BufferedReader(new FileReader(file));
 
-			if (record.indexOf(ADMIN_LEVEL) == -1) {
-				if (record.indexOf("= 0") >= 1) {
-					// System.out.println(""+name+":   "+record);
-				} else if ((record.indexOf(" = ") >= 1)&&((record.indexOf("#") == -1))) {
-					int idx = record.indexOf("=");
-					//String val = record.substring(idx + 1, record.length());
-					String key = (record.substring(0, idx)).trim();
-					//System.out.println("key = "+key);
-					try {
-						//Double obj = new Double(val.trim());
-						if(keySet.contains(key)) duplicateSet.add(key);
-						
-						keySet.add(key);
-					} catch (NumberFormatException nfe) {
-						// nothing
+			while ((record = reader.readLine()) != null) {
+				//  System.out.println(""+name+":   "+record);
+				recCount++;
+
+				if (record.indexOf(ADMIN_LEVEL) == -1) {
+					if (record.indexOf("= 0") >= 1) {
+						// System.out.println(""+name+":   "+record);
+					} else if ((record.indexOf(" = ") >= 1)&&((record.indexOf("#") == -1))) {
+						int idx = record.indexOf("=");
+						//String val = record.substring(idx + 1, record.length());
+						String key = (record.substring(0, idx)).trim();
+						//System.out.println("key = "+key);
+						try {
+							//Double obj = new Double(val.trim());
+							if(keySet.contains(key)) duplicateSet.add(key);
+
+							keySet.add(key);
+						} catch (NumberFormatException nfe) {
+							// nothing
+						}
 					}
-				}
 
-			}
-		} // while
+				}
+			} // while
 		} catch (Exception e) {
 			// catch io errors from FileInputStream or readLine() 
 			System.out.println(" IOException error!" + e.getMessage());
 			e.printStackTrace();
+		} finally {
+			try {
+				if (reader != null) {
+					reader.close();
+				}
+			} catch (Exception e) {
+				// Ignore exceptions on close
+			}
 		}
-		
+
 		String[] allKeys = new String[duplicateSet.size()];
 		Iterator<String> iter = duplicateSet.iterator();
 		int icount = 0;
@@ -231,12 +258,12 @@ public class DataUtility {
 			icount ++;
 		}
 		Arrays.sort(allKeys);
-		
+
 		for (int i = 0 ; i < allKeys.length; i ++) {
 			System.out.println(""+allKeys[i]);
 		}
-		
+
 
 	}// CHECK DUPLICATES
-	
+
 }
