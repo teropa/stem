18bdf85251d6893d936bdca8867e0754d6eff36d
Tero Parviainen <tero.parviainen@eficode.fi>
2010-10-03 21:18:42 +0300

    connection to CDO server

--- org.eclipse.stem.jobs/src/org/eclipse/stem/jobs/simulation/remote/RemoteSimulationManager.java
+++ org.eclipse.stem.jobs/src/org/eclipse/stem/jobs/simulation/remote/RemoteSimulationManager.java
@@ -1,5 +1,15 @@
 package org.eclipse.stem.jobs.simulation.remote;
 
+
+import org.eclipse.emf.cdo.net4j.CDONet4jUtil;
+import org.eclipse.emf.cdo.net4j.CDOSessionConfiguration;
+import org.eclipse.emf.cdo.session.CDOSession;
+import org.eclipse.net4j.Net4jUtil;
+import org.eclipse.net4j.tcp.ITCPConnector;
+import org.eclipse.net4j.tcp.TCPUtil;
+import org.eclipse.net4j.util.container.ContainerUtil;
+import org.eclipse.net4j.util.container.IManagedContainer;
+import org.eclipse.net4j.util.lifecycle.LifecycleUtil;
 import org.eclipse.stem.jobs.execution.IExecutable;
 
 
@@ -26,8 +36,28 @@ public class RemoteSimulationManager {
 	} // getManager
 
 	public void run(IExecutable executable) {
-		System.out.println("Would now run remotely: "+executable);
+		System.out.println("Running remotely: "+executable);
+		CDOSession session = createSession("localhost:2036", "repo1");
+		try {
+			System.out.println("last upd: "+session.getLastUpdateTime());
+		} finally {
+			session.close();
+		}
 	}
 
+	private CDOSession createSession(String url, String repo) {
+		CDOSessionConfiguration config = CDONet4jUtil.createSessionConfiguration();
+		config.setConnector(createConnector(url));
+		config.setRepositoryName(repo);
+		return config.openSession();
+	}
 
+	private ITCPConnector createConnector(String url) {
+		IManagedContainer container = ContainerUtil.createContainer();
+		TCPUtil.prepareContainer(container);
+		Net4jUtil.prepareContainer(container);
+		LifecycleUtil.activate(container);
+		ITCPConnector connector = TCPUtil.getConnector(container, url);
+		return connector;
+	}
 }
