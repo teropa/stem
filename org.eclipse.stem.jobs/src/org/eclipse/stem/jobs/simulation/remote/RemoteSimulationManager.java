600e61c2dafae27d0f277ba1d5652d9f1e516f5e
Tero Parviainen <tero.parviainen@eficode.fi>
2010-10-26 04:09:15 +0300

    CDO tx

--- org.eclipse.stem.jobs/src/org/eclipse/stem/jobs/simulation/remote/RemoteSimulationManager.java
+++ org.eclipse.stem.jobs/src/org/eclipse/stem/jobs/simulation/remote/RemoteSimulationManager.java
@@ -4,12 +4,19 @@ package org.eclipse.stem.jobs.simulation.remote;
 import org.eclipse.emf.cdo.net4j.CDONet4jUtil;
 import org.eclipse.emf.cdo.net4j.CDOSessionConfiguration;
 import org.eclipse.emf.cdo.session.CDOSession;
+import org.eclipse.emf.cdo.transaction.CDOTransaction;
+import org.eclipse.emf.cdo.util.CommitException;
+import org.eclipse.emf.common.notify.Adapter;
+import org.eclipse.emf.common.notify.Notifier;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.net4j.Net4jUtil;
 import org.eclipse.net4j.tcp.ITCPConnector;
 import org.eclipse.net4j.tcp.TCPUtil;
 import org.eclipse.net4j.util.container.ContainerUtil;
 import org.eclipse.net4j.util.container.IManagedContainer;
 import org.eclipse.net4j.util.lifecycle.LifecycleUtil;
+import org.eclipse.stem.core.scenario.Scenario;
 import org.eclipse.stem.jobs.execution.IExecutable;
 
 
@@ -36,10 +43,16 @@ public class RemoteSimulationManager {
 	} // getManager
 
 	public void run(IExecutable executable) {
-		System.out.println("Running remotely: "+executable);
+		System.out.println("Running remotely: "+executable.getClass().getName());
 		CDOSession session = createSession("localhost:2036", "repo1");
 		try {
+			CDOTransaction tx = session.openTransaction();
+			Resource resource = tx.getResourceSet().createResource(URI.createURI("/stem/simulation"));
+			resource.getContents().add((Scenario)((Adapter)executable).getTarget());
+			tx.commit();
 			System.out.println("last upd: "+session.getLastUpdateTime());
+		} catch (CommitException ce) {
+			ce.printStackTrace();
 		} finally {
 			session.close();
 		}
