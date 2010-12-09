8a09c93fe935d457f925c1ecfd1d8f7ea30e827a
jkaufman <jkaufman@92a21009-5b66-0410-b83a-dc787c41c6e9>
2010-12-09 21:35:09 +0000

    cleanup
    git-svn-id: http://dev.eclipse.org/svnroot/technology/org.eclipse.stem/trunk@1256 92a21009-5b66-0410-b83a-dc787c41c6e9

--- org.eclipse.stem.tests.automaticexperiment/src/org/eclipse/stem/tests/automaticexperiment/TestUtil.java
+++ org.eclipse.stem.tests.automaticexperiment/src/org/eclipse/stem/tests/automaticexperiment/TestUtil.java
@@ -7,14 +7,10 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
-import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IProjectDescription;
-import org.eclipse.core.resources.IResource;
-import org.eclipse.core.resources.IWorkspace;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.Platform;
 
